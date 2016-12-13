b6a835c56248c73aeae7ede983a9ffa306a80c14
onays1239210 <onays1239210@gmail.com>
2016-12-13 00:09:03 -0800

    basic workflow completed
    basic workflow completed

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+public class ChangeScene : MonoBehaviour
+{
+
+    // function click for the button
+    public void changeToScene(int changeTheScene)
+    {
+        SceneManager.LoadScene(changeTheScene);
+    }
+}
